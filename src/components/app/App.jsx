f5f062a22698708954128ab22d366eed34fb835a
Daniar02 <abykeev02@icloud.com>
2023-06-06 10:08:38 +0600

    feat: implemented character when clicked cards info

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,22 +1,34 @@
+import {Component} from "react";
 import AppHeader from "../AppHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
 
-const App = () => {
-    return (
-        <div className={'app'}>
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-            </main>
-        </div>
-    )
+class App extends Component{
+    state = {
+        selectedChar: null
+    }
+
+    onCharSelected = (id) => {
+        this.setState({
+            selectedChar: id
+        })
+    }
+    render () {
+        return (
+            <div className={'app'}>
+                <AppHeader/>
+                <main>
+                    <RandomChar/>
+                    <div className="char__content">
+                        <CharList onCharSelected={this.onCharSelected}/>
+                        <CharInfo charId={this.state.selectedChar}/>
+                    </div>
+                </main>
+            </div>
+        )
+    }
 }
 
 export default App
