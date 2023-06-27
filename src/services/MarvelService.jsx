39cf482a09a0c810bc5da850d6127de7ed008ee0
Daniar02 <abykeev02@icloud.com>
2023-06-27 22:27:39 +0600

    feat: Practice your own hooks on the project

--- src/services/MarvelService.jsx
+++ src/services/MarvelService.jsx
@@ -5,7 +5,7 @@ class MarvelService {
     getResource = async (url) => {
         let res = await fetch(url);
 
-            if (!res.ok) {
+        if (!res.ok) {
             throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
 
