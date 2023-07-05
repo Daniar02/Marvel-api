ca6fb4bc88e1a311c60569f11d25956eb08bd4b1
Daniar02 <abykeev02@icloud.com>
2023-07-05 20:47:00 +0600

    feat: added appBanner

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,6 +4,7 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import AppBanner from "../appBanner/AppBanner";
 
 import decoration from '../../resources/img/vision.png';
 
@@ -19,18 +20,19 @@ const App = () => {
         <div className={'app'}>
             <AppHeader/>
             <main>
-                <ErrorBoundary>
-                    <RandomChar/>
-                </ErrorBoundary>
-                <div className="char__content">
-                    <ErrorBoundary>
-                        <CharList onCharSelected={onCharSelected}/>
-                    </ErrorBoundary>
-                    <ErrorBoundary>
-                        <CharInfo charId={selectedChar}/>
-                    </ErrorBoundary>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                {/*<ErrorBoundary>*/}
+                {/*    <RandomChar/>*/}
+                {/*</ErrorBoundary>*/}
+                {/*<div className="char__content">*/}
+                {/*    <ErrorBoundary>*/}
+                {/*        <CharList onCharSelected={onCharSelected}/>*/}
+                {/*    </ErrorBoundary>*/}
+                {/*    <ErrorBoundary>*/}
+                {/*        <CharInfo charId={selectedChar}/>*/}
+                {/*    </ErrorBoundary>*/}
+                {/*</div>*/}
+                {/*<img className="bg-decoration" src={decoration} alt="vision"/>*/}
+                <AppBanner/>
             </main>
         </div>
     )
